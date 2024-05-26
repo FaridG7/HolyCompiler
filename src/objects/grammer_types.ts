f8b59140f26aae1133d84abc5d87955eb07ee45f
FaridG7 <goharifarid7@gmail.com>
2024-05-26 22:36:27 +0330

    refactor grammer

--- src/objects/grammer_types.ts
+++ src/objects/grammer_types.ts
@@ -1,13 +1,21 @@
 type variable = "<prog>" | "<id>" | "<dec-list>" | "<dec>" | "<type>" | "<stat-list>" | "<write>" | "<assign>" | "<expr>" | "<term>" 
     | "<factor>" | "<number>"| "<sign>" | "<digit>" | "<letter>";
-type terminal = "program" | "var" | "begin" | "end" | "integer" | "show" | ";" | ":" | "," | "(" | ")" | "=" | "+" | "-" | "*" |
-     "/" | "{" | "}" | "lambda" | "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "a" | "b" | "c" | "d" | "e";
 
-type c = terminal | variable;
+type numberTerminal = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" ;
+
+type letterTerminal = "a" | "b" | "c" | "d" | "e";
+
+type keyWordsTerminal = "program" | "var" | "begin" | "end" | "integer" | "show";
+
+type sybolTerminal = ";" | ":" | "," | "(" | ")" | "{" | "}" ;
+
+type operatorTerminal = "=" | "+" | "-" | "*" | "/";
+
+type terminal = keyWordsTerminal |letterTerminal | numberTerminal | sybolTerminal | operatorTerminal | "lambda";
 
 type rule = {
     variable: variable;
-    rightSide: c[];
+    rightSide: Array<terminal | variable>;
 }
 
 export type grammer = rule[];
