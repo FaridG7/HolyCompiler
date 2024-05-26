e2405679561fbe4e8595fa417470d1c508571d53
FaridG7 <goharifarid7@gmail.com>
2024-05-26 23:10:31 +0330

    add grammer

--- src/objects/grammer_types.ts
+++ src/objects/grammer_types.ts
@@ -1,5 +1,6 @@
 type variable = "<prog>" | "<id>" | "<dec-list>" | "<dec>" | "<type>" | "<stat-list>" | "<write>" | "<assign>" | "<expr>" | "<term>" 
-    | "<factor>" | "<number>"| "<sign>" | "<digit>" | "<letter>";
+    | "<factor>" | "<number>"| "<sign>" | "<digit>" | "<letter>" | "<alphaNum>" | "<moreDec>" 
+    | "<stat>" | "<moreStat>" | "<exprPrime>" | "<termPrime>" | "<moreDigits>";
 
 type numberTerminal = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" ;
 
@@ -11,11 +12,11 @@ type sybolTerminal = ";" | ":" | "," | "(" | ")" | "{" | "}" ;
 
 type operatorTerminal = "=" | "+" | "-" | "*" | "/";
 
-type terminal = keyWordsTerminal |letterTerminal | numberTerminal | sybolTerminal | operatorTerminal | "lambda";
+type terminal = keyWordsTerminal |letterTerminal | numberTerminal | sybolTerminal | operatorTerminal;
 
 type rule = {
     variable: variable;
-    rightSide: Array<terminal | variable>;
+    rightSide: Array<terminal | variable> | "lambda";
 }
 
 export type grammer = rule[];
