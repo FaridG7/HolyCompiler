a13a2b2b3204eeb9fc2ca7dbda534ff691c4be77
FaridG7 <goharifarid7@gmail.com>
2024-05-27 12:30:55 +0330

    add parseTable

--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -1,6 +1,16 @@
+import { Grammer } from "../assets/grammer";
 import { stack } from "../assets/stack";
 import { tokenTable } from "../assets/types/tokenTable_types";
 
 export function parser(tokenTable:tokenTable){
-    // const holderStack = new stack();
+    const holderStack = new stack();
+    holderStack.push("$");
+    holderStack.push(Grammer[0].variable);
+    while (true) {
+        //1)look at the top of the stack
+        //2.a)if it's a terminal read from the token table
+        //2.b)if it's a variable, look at the pars table
+        //3)decide based on the parsing table
+
+    }
 }
