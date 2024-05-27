30c29b79329298d80e1c2441b9d4adcb9ed68bd3
FaridG7 <goharifarid7@gmail.com>
2024-05-27 14:14:44 +0330

    change grammer format

--- src/assets/types/grammer_types.ts
+++ src/assets/types/grammer_types.ts
@@ -1,4 +1,4 @@
-export type variable = "<prog>" | "<id>" | "<dec-list>" | "<dec>" | "<type>" | "<stat-list>" | "<stat>" | "<write>" | "<assign>" | "<expr>" | "<term>" 
+type variable = "<prog>" | "<id>" | "<dec-list>" | "<dec>" | "<type>" | "<stat-list>" | "<stat>" | "<write>" | "<assign>" | "<expr>" | "<term>" 
     | "<factor>" | "<number>"| "<sign>" | "<digit>" | "<letter>" | "<alphaNum>" | "<moreDec>" | BNFtempVarables;
 
 type BNFtempVarables = "<moreStat>" | "<exprPrime>" | "<termPrime>" | "<moreDigits>";
@@ -13,11 +13,21 @@ type sybolTerminal = ";" | ":" | "," | "(" | ")" | "{" | "}" ;
 
 type operatorTerminal = "=" | "+" | "-" | "*" | "/";
 
-export type terminal = keyWordsTerminal |letterTerminal | numberTerminal | sybolTerminal | operatorTerminal | "$";
+type terminal = keyWordsTerminal |letterTerminal | numberTerminal | sybolTerminal | operatorTerminal | "$";
+
+export type terminalObject = {
+    value:terminal,
+    type:"terminal"
+}
+
+export type variableObject = {
+    value:variable,
+    type:"variable"
+}
 
 type rule = {
     variable: variable;
-    rightSide: Array<terminal | variable> | "lambda";
+    rightSide: Array<terminalObject | variableObject> | {value:"lambda",type:"terminal"};
 }
 
 export type grammer = rule[];
--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -1,12 +1,18 @@
 import { Grammer } from "../assets/grammer";
 import { stack } from "../assets/stack";
-import { tokenTable } from "../assets/types/tokenTable_types";
+import { terminal, variable } from "../assets/types/grammer_types";
+import { token, tokenTable } from "../assets/types/tokenTable_types";
+import { parseTable } from "../assets/parseTable";
 
 export function parser(tokenTable:tokenTable){
     const holderStack = new stack();
     holderStack.push("$");
     holderStack.push(Grammer[0].variable);
+    let index = 0;
     while (true) {
+        let token:token = tokenTable[index].idName;
+        let topOfStack:terminal|variable = holderStack.pop();
+        if(topOfStack.getClass() == terminal.class)
         //1)look at the top of the stack
         //2.a)if it's a terminal read from the token table
         //2.b)if it's a variable, look at the pars table
@@ -14,3 +20,5 @@ export function parser(tokenTable:tokenTable){
 
     }
 }
+
+function 
