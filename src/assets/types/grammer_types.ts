092bbadbfebd93f6f819c139d88ff7759116a634
EddyHezarian <mr.eddyhzn@gmail.com>
2024-06-04 21:58:38 +0330

    [ADD] scanner need to test on UI

--- src/assets/types/grammer_types.ts
+++ src/assets/types/grammer_types.ts
@@ -29,7 +29,7 @@ type numberTerminal = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
 
 type letterTerminal = "a" | "b" | "c" | "d" | "e";
 
-type keyWordsTerminal =
+export type keyWordsTerminal =
   | "program"
   | "var"
   | "begin"
