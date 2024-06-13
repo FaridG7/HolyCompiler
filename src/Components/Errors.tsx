0a66c9c6a78fa6212f96f27f4922a04f6a0de89a
FaridG7 <goharifarid7@gmail.com>
2024-06-13 13:26:54 +0330

    complete UI - test required

--- src/Components/Errors.tsx
+++ src/Components/Errors.tsx
@@ -1,3 +1,13 @@
+import { List, ListItem, ListItemText } from "@mui/material";
+
 export function Errors({ errors }: { errors: string[] }) {
-  return <div></div>;
+  return (
+    <List>
+      {errors.map((error) => (
+        <ListItem>
+          <ListItemText>{error}</ListItemText>
+        </ListItem>
+      ))}
+    </List>
+  );
 }
--- src/Components/TokenTable.tsx
+++ src/Components/TokenTable.tsx
@@ -1,6 +1,42 @@
-import { Box } from "@mui/material";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import { token } from "../assets/types/tokenTable_types";
 
 export function TokenTable({ tokenTable }: { tokenTable: token[] }) {
-  return <Box></Box>;
+  return (
+    <TableContainer>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Number</TableCell>
+            <TableCell>Row Number</TableCell>
+            <TableCell>Column Number</TableCell>
+            <TableCell>Block Number</TableCell>
+            <TableCell>Outer Block Number</TableCell>
+            <TableCell>Type</TableCell>
+            <TableCell>Value</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {tokenTable.map((token, index) => (
+            <TableRow key={index}>
+              <TableCell>{index + 1}</TableCell>
+              <TableCell>{token.rowNumber}</TableCell>
+              <TableCell>{token.columnNumber}</TableCell>
+              <TableCell>{token.blockNumber}</TableCell>
+              <TableCell>{token.outerBlockNumber}</TableCell>
+              <TableCell>{token.type}</TableCell>
+              <TableCell>{token.value}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
